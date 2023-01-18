5dfed0dfd4737cf78305dc21b52560937742da79
Jhenja Fedko <fedkozhenja@gmail.com>
2023-01-18 19:55:06 +0400

    feat: implement function <reverse>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let string = String(Math.abs(n));
+    let newString = '';
+
+    for(let i = string.length - 1; i >=0 ; i--) {
+        newString += string[i];
+    }
+
+  return Number(newString);
 }
